5e6ce440f8ec4b0d155befcbdcdea32557fd7e1b
John Morris <johntylermorris@jtmorris.net>
2014-09-15 16:36:50 -0700

    Attempt to remedy two popups opening when clicked.

--- assets/js/tweet-this-box.js
+++ assets/js/tweet-this-box.js
@@ -1,9 +1,14 @@
 (function($) {
 	$(document).ready(function() {
 		//	Open Tweet This links in popup instead of new browser window
-		$('a.TT_tweet_link').click(function(event) {
+		$('a.TT_tweet_link').unbind("click").click(function(event) {
 			event.preventDefault();
-			
+
+			//	Received bug report regarding double popups opening. The unbind("click")
+			//	and this event.stopPropagation() are here to make sure this is only
+			//	fired ONCE!
+			event.stopPropagation();
+
 			var url = $(this).attr('href');
 			var windowName = "Tweet This";
 			var windowSize = "width=500,height=450";
