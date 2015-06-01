1f41781abe6ad62cfeda0c31e4f5a24a53b4ab42
John Morris <johntylermorris@jtmorris.net>
2015-06-01 16:33:50 -0700

    Make sure jQuery is ready before using it.

--- assets/js/tinymce-plugin.js
+++ assets/js/tinymce-plugin.js
@@ -1,19 +1,23 @@
 /**
  * Adds a button to the TinyMCE toolbar which opens a jQuery dialog box created elsewhere.
  */
-
+console.log("TweetThis :: TinyMCE Plugin File Loaded");
+	
 (function($) {
-	tinymce.create('tinymce.plugins.tweetthis', {
-		init: function(ed, url) {
-			//	Now, create a button that opens the dialog.
-			ed.addButton('tweetthis_button', {
-				title: 'Add Tweet This Box',
-				image: url + '/../images/tinymce-button.png',
-				onclick: function() {
-					$('#TT-shortcode-creator-dialog').dialog("open");
-				}
-			});
-		}
+	$(document).ready(function() {
+		tinymce.create('tinymce.plugins.tweetthis', {
+			init: function(ed, url) {
+				console.log("TweetThis :: TinyMCE Plugin Init");
+				//	Now, create a button that opens the dialog.
+				ed.addButton('tweetthis_button', {
+					title: 'Add Tweet This Box',
+					image: url + '/../images/tinymce-button.png',
+					onclick: function() {
+						$('#TT-shortcode-creator-dialog').dialog("open");
+					}
+				});
+			}
+		});
+		tinymce.PluginManager.add('tweetthis', tinymce.plugins.tweetthis);
 	});
-	tinymce.PluginManager.add('tweetthis', tinymce.plugins.tweetthis);
 })(jQuery);
