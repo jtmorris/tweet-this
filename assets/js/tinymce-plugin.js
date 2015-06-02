30665c81b3d9e08a6f36a175576a478d827cb08a
John Morris <johntylermorris@jtmorris.net>
2015-06-02 13:36:18 -0700

    Fix console message style discrepancy.

--- assets/js/tinymce-plugin.js
+++ assets/js/tinymce-plugin.js
@@ -1,13 +1,13 @@
 /**
  * Adds a button to the TinyMCE toolbar which opens a jQuery dialog box created elsewhere.
  */
-console.log("TweetThis :: TinyMCE Plugin File Loaded");
+console.log("Tweet This :: TinyMCE Plugin File Loaded");
 	
 (function($) {
 	$(document).ready(function() {
 		tinymce.create('tinymce.plugins.tweetthis', {
 			init: function(ed, url) {
-				console.log("TweetThis :: TinyMCE Plugin Init");
+				console.log("Tweet This :: TinyMCE Plugin Init");
 				//	Now, create a button that opens the dialog.
 				ed.addButton('tweetthis_button', {
 					title: 'Add Tweet This Box',
